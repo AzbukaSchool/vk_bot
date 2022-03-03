fd6988fd50061f4acf43850b7b321ab4a3f1c5cd
SoulHunteer <seregalutovinov@yandex.ru>
2022-03-03 12:14:47 +0300

    First message

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import vk_api
 import random
 from vk_api.bot_longpoll import VkBotLongPoll
 
-vk_session = vk_api.VkApi(token='5b9b4e956cc67a7aac20e6bad3fd14b70ef01da1124ac32b81898edf7782e24fa605878a1a5640181e373')
-longpoll_bot = VkBotLongPoll(vk_session, 205343326)
+vk_session = vk_api.VkApi(token='00512b82138f907e4a1ae4b3e93a274f001260f8fab92a95efb4803b44803507810ee3df025133ad28b83')
+longpoll_bot = VkBotLongPoll(vk_session, 210994750)
 vk = vk_session.get_api()
 
 for event in longpoll_bot.listen():
-         if event.object.text == '!статус':
-                    vk.messages.send(
-                        random_id=random.getrandbits(30),
-                        peer_id=event.object.peer_id,
-                        message='Запись идёт!',
-                    )
+         print(event)
+         if event.object.text == 'Hello!':
+            vk.messages.send(
+            random_id=random.getrandbits(30),
+            peer_id=event.object.peer_id,
+            message='Hello!',
+            )
